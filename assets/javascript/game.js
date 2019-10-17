d5650d0a07d0eda64115a1b6eb1fdf10fc913290
Ricky Chen <chenricky@gmail.com>
2019-10-17 10:28:41 -0700

    made the first click with score working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
+
+var clickScore = 0;
+var totalScore = 0;
+
 $("#blueGemImg").on("click", function() {
-        alert("Blue Gem have been clicked!");
+    alert("Blue Gem have been clicked! you got : " + clickScore + " points" );
+    clickScore = Math.floor(Math.random()*100);
+    totalScore = totalScore + clickScore;
+    $("#totalScore").html("Total score is: " + totalScore);
 });
+
+
 })
