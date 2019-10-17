a589a5e9c44d46f27e98e56b551ddfd343cdeda7
Ricky Chen <chenricky@gmail.com>
2019-10-17 14:38:03 -0700

    one iteration fully working, but compure score wont change after loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,77 +1,117 @@
 $(document).ready(function () {
 
 var totalScore = 0;
-var winCount = 0;
-var lossCount = 0;
-var computerScore = Math.floor(Math.random()*410);
-console.log(computerScore);
+var winCount = 1;
+var lossCount = 1;
+var computerScore;
+function randomComputerScore () {
+    var computerScore = Math.floor(Math.random()*410);
+    //console.log("computer score is :" + computerScore);
+    return computerScore;
+}
+
+//randomComputerScore();
+var computerScoreNew = randomComputerScore();
+//console.log("random computer score from a func is " + randomComputerScore());
+console.log("computer score should not change in the same iteration: " + computerScoreNew);
 
 var lossStatus = false;
 
 if (lossStatus = true) {
-    var computerScore = Math.floor(Math.random()*410);
+    //var computerScore = Math.floor(Math.random()*410);
+    randomComputerScore();
 }
 
 $("#blueGemImg").on("click", function() {
     var clickScore = Math.floor(Math.random()*100);
     alert("Blue Gem have been clicked! you got : " + clickScore + " points" );
     totalScore = totalScore + clickScore;
+    console.log("blue click score is " + clickScore);
+    console.log("total score is " + totalScore);
+    console.log("computer score is " + randomComputerScore());
+    console.log("computer score should not change in the same iteration: " + computerScoreNew);
     
-    if (totalScore < computerScore) {
+    if (totalScore < computerScoreNew) {
         $("#winCount").html("winCount is: " + winCount++);
+        console.log(lossStatus + " from blue gem If block");
     }
     else {
-        $("#lossCount").html("lossCOunt is: " + lossCount++);
+        $("#lossCount").html("lossCount is: " + lossCount++);
         lossStatus = true;
-    }
-
-    $("#totalScore").html("Total score is: " + totalScore);
-});
-
-$("#purpleGemImg").on("click", function() {
-    var clickScore = Math.floor(Math.random()*50);
-    alert("purple Gem have been clicked! you got : " + clickScore + " points" );
-    totalScore = totalScore + clickScore;
-    
-    if (totalScore < computerScore) {
-        $("#winCount").html("winCount is: " + winCount++);
-    }
-    else {
-        $("#lossCount").html("lossCount is : " + lossCount++);
+        console.log(lossStatus + " from blue gem ELSE block");
+       
     }
 
     $("#totalScore").html("Total score is: " + totalScore);
 });
 
 $("#pinkGemImg").on("click", function() {
-    var clickScore = Math.floor(Math.random()*250);
-    alert("purple Gem have been clicked! you got : " + clickScore + " points" );
+    var clickScore = Math.floor(Math.random()*100);
+    alert("Pink Gem have been clicked! you got : " + clickScore + " points" );
     totalScore = totalScore + clickScore;
+    console.log("blue click score is " + clickScore);
+    console.log("total score is " + totalScore);
+    console.log("computer score is " + randomComputerScore());
+    console.log("computer score should not change in the same iteration: " + computerScoreNew);
     
-    if (totalScore < computerScore) {
+    if (totalScore < computerScoreNew) {
         $("#winCount").html("winCount is: " + winCount++);
+        console.log(lossStatus + " from pink gem If block");
     }
     else {
         $("#lossCount").html("lossCount is: " + lossCount++);
+        lossStatus = true;
+        console.log(lossStatus + " from pink gem ELSE block");
+       
+    }
+
+    $("#totalScore").html("Total score is: " + totalScore);
+});
+
+$("#purpleGemImg").on("click", function() {
+    var clickScore = Math.floor(Math.random()*100);
+    alert("Purple Gem have been clicked! you got : " + clickScore + " points" );
+    totalScore = totalScore + clickScore;
+    console.log("purple click score is " + clickScore);
+    console.log("total score is " + totalScore);
+    console.log("computer score is " + randomComputerScore());
+    console.log("computer score should not change in the same iteration: " + computerScoreNew);
+    
+    if (totalScore < computerScoreNew) {
+        $("#winCount").html("winCount is: " + winCount++);
+        console.log(lossStatus + " from purple gem If block");
+    }
+    else {
+        $("#lossCount").html("lossCount is: " + lossCount++);
+        lossStatus = true;
+        console.log(lossStatus + " from ppourple gem ELSE block");
+      
     }
 
     $("#totalScore").html("Total score is: " + totalScore);
 });
 
 $("#threeGemImg").on("click", function() {
-    var clickScore = Math.floor(Math.random()*10);
-    alert("purple Gem have been clicked! you got : " + clickScore + " points" );
+    var clickScore = Math.floor(Math.random()*100);
+    alert("Three Gem have been clicked! you got : " + clickScore + " points" );
     totalScore = totalScore + clickScore;
+    console.log("blue click score is " + clickScore);
+    console.log("total score is " + totalScore);
+    console.log("computer score is " + randomComputerScore());
+    console.log("computer score should not change in the same iteration: " + computerScoreNew);
     
-    if (totalScore < computerScore) {
+    if (totalScore < computerScoreNew) {
         $("#winCount").html("winCount is: " + winCount++);
+        console.log(lossStatus + " from three gem If block");
     }
     else {
-        $("#lossCount").html("lostCount is: " + lossCount++);
+        $("#lossCount").html("lossCount is: " + lossCount++);
+        lossStatus = true;
+        console.log(lossStatus + " from three gem ELSE block");
+    
     }
 
     $("#totalScore").html("Total score is: " + totalScore);
 });
-
 
 })
